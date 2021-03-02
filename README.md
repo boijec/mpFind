d45729875f07e1980fbef1659b93972bc6085be7
Carl Boije <boijec@gmail.com>
2021-03-02 18:59:37 +0100

    main function refactor and readme change

--- README.md
+++ README.md
@@ -10,5 +10,19 @@ Use the package manager [pip](https://pip.pypa.io/en/stable/) to install **_pyth
 pip install python-nmap
 ```
 
+##Usage
+
+To run the script, navigate to the main directory of mpFind.py and then run it using:
+
+```bash
+py mpFind.py 
+```
+
+The script takes a single input argument - **your ip address with the last localization number replaced with a '*'**
+Example:
+```bash
+py mpFind.py 10.423.88.*
+```
+
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- mpFind.py
+++ mpFind.py
@@ -2,18 +2,18 @@ import sys
 from modules import helperFunctions
 from modules import nmapper
 
-def main(gate):
+def main():
     try:
-        nmapper.NmapScan(gate)
+        helperFunctions.splash()
+        gateway = nmapper.host()
+
+        if(len(sys.argv) > 1):
+            nmapper.NmapScan(sys.argv[1])
+        else:
+            raise Exception()
     except:
         helperFunctions.bad_splash(gateway)
 
 
 if __name__ == "__main__":
-    helperFunctions.splash()
-    gateway = nmapper.host()
-
-    if(len(sys.argv) > 1):
-        main(sys.argv[1])
-    else:
-        helperFunctions.bad_splash(gateway)
+    main()
